cb8ebd4f509726c7a030985a9fa65e18c0dfb35f
Pedro Martinez Lopez <61512897+Ped-Marlo@users.noreply.github.com>
2020-07-08 09:08:12 +0200

    Add files via upload

---
+++ deleter.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:53:06 2020
+
+@author: c40441
+"""
+
+import  shutil
+
+shutil.rmtree(r'Z:\aie_load\A350\A350-XWB\Bricks\Site-Componentes\LGD\Multi-conf\Becario_Pedro\1000\Left\BayesianRidge',ignore_errors=True)
