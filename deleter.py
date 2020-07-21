9f7e13a958a951429b48d545c5f1fde2fd35b77e
Pedro Martinez Lopez <61512897+Ped-Marlo@users.noreply.github.com>
2020-07-21 09:33:31 +0200

    Update deleter.py

--- deleter.py
+++ deleter.py
@@ -2,9 +2,9 @@
 """
 Created on Tue Mar 10 13:53:06 2020
 
-@author: c40441
+@author: Pedro Martinez 
 """
 
 import  shutil
-
-shutil.rmtree(r'Z:\aie_load\A350\A350-XWB\Bricks\Site-Componentes\LGD\Multi-conf\Becario_Pedro\1000\Left\BayesianRidge',ignore_errors=True)
+folderpath = r'Z:\aie_load\A350\A350-XWB\Bricks\Site-Componentes\LGD\Multi-conf\Becario_Pedro\1000\Left\BayesianRidge'
+shutil.rmtree(folderpath ,ignore_errors=True)
