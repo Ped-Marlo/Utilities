84c3691e37694bbfd267ad373fa34ff36dcd2c87
Pedro Martinez Lopez <61512897+Ped-Marlo@users.noreply.github.com>
2020-07-21 10:46:38 +0200

    Update File_lister.py

--- File_lister.py
+++ File_lister.py
@@ -2,23 +2,29 @@
 """
 Created on Tue Jul 21 09:35:37 2020
 
-@author: ng8569f
+@author: Pedro Martinez
 """
 from os import listdir
 from os.path import isfile, join
 
-#folderpath = r'Z:\aie_load\A350\A350-XWB\Bricks\Site-Componentes\LGD\Multi-conf\Becario_Pedro\archivos_explorar\1024.zip'
-folderpath = r'Z:\aie_load\A350\A350-XWB'
-if folderpath.endswith('.zip'):
+folderpath = r'Z:\aie_load\A350\A350-XWB\Bricks\Site-Componentes\LGD\Multi-conf\Becario_Pedro\archivos_explorar\1024.zip'
+#folderpath = r'Z:\aie_load\A350\A350-XWB'
+extension = "cdf"
+
+def search (folderpath= folderpath, extension=extension):
+    if folderpath.endswith('.zip'):
         import zipfile
         ziper = zipfile.ZipFile(folderpath)
         all_files = ziper.namelist()
+        ext_files = [f for f in all_files if f.endswith(extension)]
         
         
+    else:
+        import glob
+        all_files = [f for f in listdir(folderpath) if isfile(join(folderpath, f))]
         
-else:
-    import glob
-    extension = "*.pdf"
-    all_files = [f for f in listdir(folderpath) if isfile(join(folderpath, f))]
-    
-    ext_files = glob.glob(folderpath+"\\"+extension)
+        ext_files = glob.glob(folderpath+"\\*."+extension)
+        
+    return all_files, ext_files
+
+all_files, ext_files = search()
